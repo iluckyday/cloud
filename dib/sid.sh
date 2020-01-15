20262930b31a54e091f0c4932cd5c08e2588cd0b
iluckyday <luckyday1990@mail.com>
2020-01-15 02:54:00 +0000

    test

---
+++ dib/sid.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+set -e
+
+WORKDIR=$(mktemp -t -d sid-XXXX)
+chmod 755 $WORKDIR
+mount -t tmpfs -o size=80% tmpfs $WORKDIR
+
+mkdir -p $WORKDIR/files $WORKDIR/files/home/debian $WORKDIR/files/etc/{dpkg/dpkg.cfg.d,apt/apt.conf.d} $WORKDIR/files/etc/systemd/{system,network,journald.conf.d} $WORKDIR/elements/diy/{extra-data.d,cleanup.d}
+
+cat << "EOF" > $WORKDIR/elements/diy/extra-data.d/99-zz-diy
+#!/bin/bash
+sudo rm -f $TMP_HOOKS_PATH/*/*-cloud-init $TMP_HOOKS_PATH/*/*-debian-networking $TMP_HOOKS_PATH/*/*-baseline-environment
+sudo sed -i 's/vga=normal/quiet ipv6.disable=1 intel_iommu=on/' $TMP_HOOKS_PATH/*/*-bootloader
+EOF
+chmod +x $WORKDIR/elements/diy/extra-data.d/99-zz-diy
+
+cat << "EOF" > $WORKDIR/elements/diy/cleanup.d/99-zz-diy
+#!/bin/bash
+export TARGET_ROOT
+export basedir=$(dirname ${ELEMENTS_PATH%%:*})
+find ${basedir}/files -type f -exec bash -c 'dirname {} | sed -e "s@${basedir}/files@@" | xargs -I % bash -c "mkdir -p $TARGET_ROOT%; sudo cp {} $TARGET_ROOT%"' \;
+
+sudo touch $TARGET_ROOT/home/${DIB_DEV_USER_USERNAME}/.hushlogin
+echo -e "\n\nexport HISTSIZE=1000 LESSHISTFILE=/dev/null HISTFILE=/dev/null"| sudo tee -a $TARGET_ROOT/home/${DIB_DEV_USER_USERNAME}/.bashrc
+echo debian | sudo tee $TARGET_ROOT/etc/hostname
+echo -e "\n\nnet.core.default_qdisc=fq\nnet.ipv4.tcp_congestion_control=bbr" | sudo tee -a $TARGET_ROOT/etc/sysctl.conf
+sed -i '/src/d' $TARGET_ROOT/etc/apt/sources.list
+
+sudo chroot $TARGET_ROOT systemctl enable systemd-networkd
+sudo chroot $TARGET_ROOT systemctl -f mask apt-daily.timer apt-daily-upgrade.timer fstrim.timer motd-news.timer
+
+#sudo chroot $TARGET_ROOT apt remove --purge -y 
+
+sudo rm -rf $TARGET_ROOT/etc/dib-manifests $TARGET_ROOT/var/log/* $TARGET_ROOT/usr/share/doc/* $TARGET_ROOT/usr/share/man/* $TARGET_ROOT/tmp/* $TARGET_ROOT/var/tmp/* $TARGET_ROOT/var/cache/apt/*
+sudo find $TARGET_ROOT/usr/share/locale -mindepth 1 -maxdepth 1 ! -name 'en' -exec rm -rf {} +
+EOF
+
+chmod +x $WORKDIR/elements/diy/cleanup.d/99-zz-diy
+
+cat << EOF > $WORKDIR/files/etc/fstab
+LABEL=cloudimg-rootfs /    ext4  defaults,noatime                            0 0
+tmpfs                 /tmp tmpfs mode=1777,strictatime,nosuid,nodev,size=90% 0 0
+EOF
+
+cat << EOF > $WORKDIR/files/etc/apt/apt.conf.d/99-freedisk
+APT::Authentication "0";
+APT::Get::AllowUnauthenticated "1";
+Dir::Cache "/dev/shm";
+Dir::State::lists "/dev/shm";
+Dir::Log "/dev/shm";
+DPkg::Post-Invoke {"/bin/rm -f /dev/shm/archives/*.deb || true";};
+EOF
+
+cat << EOF > $WORKDIR/files/etc/dpkg/dpkg.cfg.d/99-nodoc
+path-exclude /usr/share/doc/*
+path-exclude /usr/share/man/*
+path-exclude /usr/share/groff/*
+path-exclude /usr/share/info/*
+path-exclude /usr/share/lintian/*
+path-exclude /usr/share/linda/*
+path-exclude /usr/share/locale/*
+path-include /usr/share/locale/en*
+EOF
+
+cat << EOF > $WORKDIR/files/etc/systemd/journald.conf.d/storage.conf
+[Journal]
+Storage=volatile
+EOF
+
+cat << EOF > $WORKDIR/files/etc/systemd/network/20-dhcp.network
+[Match]
+Name=en*
+
+[Network]
+DHCP=ipv4
+EOF
+
+sed -i 's/4096/16384 -O ^has_journal/' `python3 -c "import os,diskimage_builder; print(os.path.dirname(diskimage_builder.__file__))"`/lib/disk-image-create
+
+sed -i 's/linux-image-amd64/linux-image-cloud-amd64/' `python3 -c "import os,diskimage_builder; print(os.path.dirname(diskimage_builder.__file__))"`/elements/debian-minimal/package-installs.yaml
+
+
+#DIB_QUIET=1 \
+DIB_IMAGE_SIZE=20 \
+DIB_JOURNAL_SIZE=0 \
+DIB_EXTLINUX=1 \
+ELEMENTS_PATH=$WORKDIR/elements \
+DIB_RELEASE=sid \
+DIB_APT_MINIMAL_CREATE_INTERFACES=0 \
+DIB_DEBOOTSTRAP_EXTRA_ARGS+=" --no-check-gpg" \
+DIB_DEBOOTSTRAP_EXTRA_ARGS+=" --include=bash-completion,iproute2" \
+DIB_DEBOOTSTRAP_EXTRA_ARGS+=" --exclude=unattended-upgrades" \
+DIB_DEV_USER_USERNAME=debian \
+DIB_DEV_USER_PASSWORD=debian \
+DIB_DEV_USER_SHELL=/bin/bash \
+DIB_DEV_USER_PWDLESS_SUDO=yes \
+DIB_DEBOOTSTRAP_DEFAULT_LOCALE=en_US.UTF-8 \
+disk-image-create -o $WORKDIR/sid.qcow2 vm block-device-mbr cleanup-kernel-initrd devuser diy debian-minimal
+
+echo "Original image size:"
+ls -lh $WORKDIR/sid.qcow2
+
+echo Converting ...
+qemu-img convert -f qcow2 -c -O qcow2 $WORKDIR/sid.qcow2 /dev/shm/sid-`date "+%Y%m%d"`.cmp.img
+
+echo "Compressed image size:"
+ls -lh /dev/shm/sid-*.cmp.img
+
+sync
+umount $WORKDIR
+
+exit 0
