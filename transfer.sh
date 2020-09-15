ca08004cac52b57ede1e567389acef32e1b5e793
iluckyday <luckyday1990@mail.com>
2020-09-16 00:20:10 +0800

    test

--- transfer.sh
+++ transfer.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -e
 
-cow_ver="$(curl -skL https://api.github.com/repos/Mikubill/transfer/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')"
-curl -skL https://github.com/Mikubill/transfer/releases/download/"$cow_ver"/transfer_"${cow_ver/v/}"_linux_amd64.tar.gz | tar -xz -C /tmp
-#chmod +x /tmp/transfer
+ver="$(curl -skL https://api.github.com/repos/Mikubill/transfer/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')"
+curl -skL https://github.com/Mikubill/transfer/releases/download/"$ver"/transfer_"${ver/v/}"_linux_amd64.tar.gz | tar -xz -C /tmp
 
 FILE=$1
 FILENAME=$(basename $FILE)
