4b24a74a32fc64d6d324f8aff2e2c5304fdc0d68
iluckyday <luckyday1990@mail.com>
2020-09-16 00:17:08 +0800

    test

--- cowtransfer.sh
+++ cowtransfer.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -e
 
-cow_ver="$(curl -skL https://api.github.com/repos/Mikubill/cowtransfer-uploader/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')"
-curl -skL https://github.com/Mikubill/cowtransfer-uploader/releases/download/"$cow_ver"/cowtransfer-uploader_"${cow_ver/v/}"_linux_amd64.tar.gz | tar -xz -C /tmp
-#chmod +x /tmp/cowtransfer-uploader
+cow_ver="$(curl -skL https://api.github.com/repos/Mikubill/transfer/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')"
+curl -skL https://github.com/Mikubill/transfer/releases/download/"$cow_ver"/transfer_"${cow_ver/v/}"_linux_amd64.tar.gz | tar -xz -C /tmp
+#chmod +x /tmp/transfer
 
 FILE=$1
 FILENAME=$(basename $FILE)
 SIZE="$(du -h $FILE | awk '{print $1}')"
-cow_data=$(/tmp/cowtransfer-uploader --silent $FILE)
+cow_data=$(/tmp/transfer cow --silent $FILE)
 cow_url=$(echo $cow_data | cut -d' ' -f2)
 data="$FILENAME-$SIZE-${cow_url}"
 echo $data
